84884ccd919a1c220afec6f9cbf95d88297265a8
Allen Shin <geeuho@gmail.com>
2019-11-04 23:40:11 -0800

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,43 @@
 class Instructor
+    @@all =[]
+    attr_accessor :name
 
+    def initialize (name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def pass_student(student, test_name)
+        all_tests = BoatingTest.all.map { |n|
+            if n.student == student && n.test_name == test_name 
+                return n.test_status = "passed"
+            end
+        }
+        if all_tests.include?("passed") == false
+            BoatingTest.new(student, test_name, "passed", self)
+        end
+        
+    end
+
+    def fail_student(student, test_name)
+        all_tests = BoatingTest.all.map { |n|
+            if n.student == student && n.test_name == test_name 
+                return n.test_status = "failed"
+            end
+        }
+        if all_tests.include?("failed") == false
+            BoatingTest.new(student, test_name, "failed", self)
+        end
+        
+    end
 end
+
+
+# Instructor#pass_student should take in a student instance and test name. 
+# If there is a BoatingTest whose name and student match the values passed in, this method should update the status of that BoatingTest to 'passed'.
+# If there is no matching test, this method should create a test with the student, that boat test name, and the status 'passed'. 
+# Either way, it should return the BoatingTest instance.
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,41 @@
 class Student
+    @@all = []
+    attr_accessor :first_name
+
+    def initialize (first_name)
+        @first_name = first_name
+        @@all << self
+    end
+
+    def add_boating_test(student, stest_name, test_status, instructor)
+        BoatingTest.new(self, test_name, test_status, instructor)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_student(name)
+        @@all.select { |n|
+            n.first_name == name
+        }
+    end
+
+    def grade_percentage
+        pass_count = 0
+        tests = BoatingTest.all.select { |n|
+            n.student == self
+        }
+        test_count = tests.length
+        tests.map { |n|
+            if n.test_status == "passed"
+                pass_count += 1
+            end
+        }
+        pp pass_count
+        pp test_count
+        return pass_count/test_count.to_f * 100
+
+    end
 
 end
