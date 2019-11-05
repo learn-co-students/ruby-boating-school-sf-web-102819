5a390e68e02edbed2bfa98c06f4b1501c65cc22c
mwong068 <mwong068@gmail.com>
2019-11-04 16:15:43 -0800

    Done

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,43 @@
 class Instructor
 
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def pass_student(student, test_name)
+        matching_test = BoatingTest.all.select {|test| test.student.first_name == student && test.test_name == test_name}
+            if matching_test.length > 0
+                matching_test.each do |test|
+                    test.test_status = "passed"
+                end
+            else
+                BoatingTest.new(student, test_name, "passed", self)
+            end
+    end   
+
+    def fail_student(student, test_name)
+        matching_test = BoatingTest.all.select {|test| test.student.first_name == student && test.test_name == test_name}
+        if matching_test.length > 0
+            matching_test.each do |test|
+                test.test_status = "failed"
+            end
+        else
+            BoatingTest.new(student, test_name, "failed", self)
+        end
+    end
+
 end
+
+
+
+# * `Instructor#pass_student` should take in a student instance and test name. If there is a `BoatingTest` whose name and student match the values passed in, this method should update the status of that BoatingTest to 'passed'. If there is no matching test, this method should create a test with the student, that boat test name, and the status 'passed'. Either way, it should return the `BoatingTest` instance.
+# * `Instructor#fail_student` should take in a student instance and test name. Like `#pass_student`, it should try to find a matching `BoatingTest` and update its status to 'failed'. If it cannot find an existing `BoatingTest`, it should create one with the name, the matching student, and the status 'failed'.
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,44 @@
 class Student
 
+    attr_reader :first_name
+
+    @@all = []
+
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    # * `Student#add_boating_test` should initialize a new boating test with a Student (Object), a boating test name (String), a boating test status (String), and an Instructor (Object)
+
+    def add_boating_test(test_name, test_status, instructor)
+        BoatingTest.new(self, test_name, test_status, instructor)
+    end
+
+
+# * `Student.find_student` will take in a first name and output the student (Object) with that name
+
+    def self.find_student(name)
+        self.all.find {|student| student.first_name == name}
+    end
+
+
+# * `Student#grade_percentage` should return the percentage of tests that the student has passed, a Float (so if a student has passed 3 / 9 tests that they've taken, this method should return the Float `33.33`)
+
+    def grade_percentage
+        total = 0
+        student_tests = BoatingTest.all.select {|test| test.student == self}
+        student_tests.each do |test|
+            if test.test_status == "passed"
+                total += 1
+            end
+        end
+        average = total.to_f / student_tests.length
+        average * 100
+    end
+
 end
